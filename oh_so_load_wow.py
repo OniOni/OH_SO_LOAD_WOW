990b35233fc3a2f4a22683046463cfbc0f4c311f
OniOni <mathieu.c.sabourin@gmail.com>
2013-11-20 18:10:41 -0800

    Centralize logging + use file for urls.

--- oh_so_load_wow.py
+++ oh_so_load_wow.py
@@ -1,19 +1,27 @@
+import time
+
 import monocle
 
 monocle.init("twisted")
 from monocle.stack import eventloop
-
 from monocle import _o, Return
+from monocle.experimental import Channel
 
 @_o
-def p0wn(url):
+def p0wn(url, c):
     from monocle.stack.network.http import HttpClient, HttpHeaders
     from urlparse import urlparse
     from base64 import encodestring
 
     o = urlparse(url)
     client = HttpClient()
-    yield client.connect(o.hostname, o.port, scheme=o.scheme)
+
+    print o
+    try:
+        yield client.connect(o.hostname, getattr(o, 'port', 80), scheme=o.scheme)
+    except Exception:
+        print "error", url
+        yield Return("Error")
 
     if hasattr(o, 'username'):
         headers = HttpHeaders({
@@ -21,19 +29,48 @@ def p0wn(url):
         })
 
     i = 0
+    mean = 0
+    total = 0
     while True:
+        then = time.time()
         res = yield client.request(o.path, headers=headers or None)
+        now = time.time()
+
         i += 1
-        print "%s -- %s -- %s" % (url, res.body, i)
+        duration = now - then
+        total += duration
+        mean = total / i
+
+        c.send({'url': url, 'duration': duration, 'count': i, 'mean': mean})
+
+@_o
+def pprint(c):
+    import os
+
+    _all = {}
+
+    then = time.time()
+    while True:
+        d = yield c.recv()
+        _all[d['url']] = d
+
+        if time.time() - then > 2.0:
+            os.system('clear')
+            then = time.time()
+            for k, v in _all.items():
+                print " %s -- %s -- %s for %s calls" % (k, v['duration'], v['mean'], v['count'])
+
 
 if __name__ == '__main__':
-    to_pawn = [
-        'http://admin:0e779f56-385a-41be-a562-6f6908bf5acf@mathieu.dev.saucelabs.net:8842/rest/v1/admin/tunnels',
-        'http://mathieu.dev.saucelabs.net:8842/rest/v1/info/counter',
-        'http://mathieu.dev.saucelabs.net:8842/rest/v1/hello'
-    ]
+    with open('to_pawn.txt') as f:
+        p = f.read()
+
+    to_pawn = p.split(',')
+
+    c = Channel(32)
+    monocle.launch(pprint, c)
 
     for url in to_pawn:
-        monocle.launch(p0wn, url)
+        monocle.launch(p0wn, url.strip(), c)
 
     eventloop.run()
